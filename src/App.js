74458ed9f4a68b88462d4ed5fe4a49988c8c4306
Shashank022 <shashank022@gmail.com>
2020-01-28 21:22:09 -0500

    Added new changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import Todos from './components/Todos';
+import Ninjas from './Ninjas';
 import './App.css';
 
 class App extends React.Component {
-state = {
-  todos:[
-    {
-      id:1,
-      title:'Take the things from Publix',
-      completed:false
-    },
-    {
-      id:2, 
-      title:'Take the things from Walmart',
-      completed:false
-    },
-    {
-      id:3,
-      title:'Take the things from Target',
-      completed:false
-    },
-  ]
-}
-
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}></Todos>
+      <h2>My First App </h2>
+      <p>Welcome......!!!</p>
+      <Ninjas/>
       </div>
     );
   }
---
+++ src/Ninjas.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class Ninjas extends React.Component {
+  render() {
+    return (
+      <div className="ninja">
+      <div>Name: Ryu </div>
+      <div>Age : 29</div>
+      <div>Belt : Black</div>
+      </div>
+    );
+  }
+}
+
+export default Ninjas;
