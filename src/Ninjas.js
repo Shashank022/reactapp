486fa0fd4fdf236fbc3d130411b3764ec6726354
Shashank022 <shashank022@gmail.com>
2020-01-30 12:00:31 -0500

    Added new chnages to Ninjas

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -5,20 +5,33 @@ const Ninjas= ({ninjas}) =>{
   
     const ninjaList = ninjas.map(ninja =>{
 
-      if(ninja.age >20){
+      if(ninja.age >40){
         return (
           <div className="ninja" key={ninja.id}>
               <div>Name: {ninja.name}</div>
               <div>Age : {ninja.age}</div>
               <div>Belt; {ninja.belt}</div>
+              <div><br></br></div>
           </div>
+
           )
         }else{
           return null;
         }
-
       });
       
+      // const ninjaList = ninjas.map(ninja =>{
+      //    return ninja.age >40 ? (
+      //   <div className="ninja" key={ninja.id}>
+      //          <div>Name: {ninja.name}</div>
+      //          <div>Age : {ninja.age}</div>
+      //          <div>Belt; {ninja.belt}</div>
+      //          <div><br></br></div>
+      //   </div>
+      //    ) : null;
+      // })
+
+
     return (
       <div className="ninja">
         {ninjaList}
