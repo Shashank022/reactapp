da93bededd9e8e734c67065ed3ad2afa63cb3570
Shashank022 <shashank022@gmail.com>
2020-02-29 23:07:17 -0500

    added new Person object

---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const person = (props) =>{
+    return(
+        <div>
+            <p>I am the Person with Name {props.name} and with the age of {props.age}</p>
+        </div>
+    ) 
+    
+}
