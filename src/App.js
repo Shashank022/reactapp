e1e7a675d56644b4ea19c233c0357bb2189fd8a3
Shashank <shashank022@gmail.com>
2020-03-02 14:38:20 -0500

    New changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
         showPersons:false
   }); 
 
-
 switchNameHandler = (newName) => {
   this.setState({
     persons:[
@@ -30,8 +29,6 @@ togglePersonsHandler = () => {
   this.setState({showPersons: !doesShow})
 
 }
-
-
 
 nameChangeHandler = (event) => {
   this.setState({
@@ -50,8 +47,7 @@ render(){
         <h1>REACT</h1>
         <p>This is working...!!!</p>
         <button onClick={() => this.togglePersonsHandler}>Switch Prop</button>
-        {
-          this.showPersons === true ?
+        
           <div>
             <Person 
                 name={this.state.persons[0].name} 
@@ -66,35 +62,10 @@ render(){
             <Person 
                 name={this.state.persons[2].name} 
                 age={this.state.persons[2].age}/>
-          </div> : null
-          }
+          </div>
     </div>
   );
 }
-    //return React.createElement('div',null,React.createElement('h1',{className:'App'},'New React App...!!'));
 }
 
 export default App;
-
-
-// state = {
-//   persons:[
-//     { name:'Kumar', age:28 },
-//     { name:'Kiran', age:29 },
-//     { name:'Lenin', age:22 }
-//   ],
-//   otherState:'some other value'
-  
-// }
-
-// switchNameHandler = () => {
-//   this.setState({
-//     persons:[
-//       { name:'Raja Kumar', age:89 },
-//       { name:'Kiran Reddy', age:29 },
-//       { name:'Lenin Smith', age:45 }
-//     ]
-    
-//   });
-//   console.log(this.state);
-// }
