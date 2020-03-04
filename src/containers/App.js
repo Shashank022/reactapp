5eff54a5f46dff44be4d30e26fa10bc1583adf5a
Shashank <shashank022@gmail.com>
2020-03-04 10:14:13 -0500

    New Changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 //import Radium  from 'radium';
-import Person from './Person/Person';
+import Person from '../Persons/Person/Person';
 
 
 class App extends Component {
